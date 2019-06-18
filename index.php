b7930532cb3de0432a3e651fa064c33ddb1ad0af
Abizon <33980864+Abizon@users.noreply.github.com>
2019-06-18 12:03:13 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ЯЭксперт</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+</head>
+
+<body>
+    <div id="preload"><span class="load"></span></div>
+    <?php include "pop.html"; ?>
+
+    <header>
+        <div class="navbar">
+            <a href="#" class="logo"></a>
+            <ul class="menu">
+                <a href="#" class="link_menu">
+                    <li>ГЛАВНАЯ</li>
+                </a>
+                <a href="#" class="link_menu">
+                    <li>О НАС</li>
+                </a>
+                <a href="#" class="link_menu">
+                    <li>НАЙТИ ЭКСПЕРТА</li>
+                </a>
+                <a href="#" class="link_menu">
+                    <li>СТАТЬ ЭКСПЕРТОМ</li>
+                </a>
+                <a href="#" class="link_menu">
+                    <li>КОНТАКТЫ</li>
+                </a>
+                <a href="#" class="burger">
+                    <i class="fas fa-bars"></i>
+                </a>
+
+            </ul>
+            <div class="profile">
+                <div class="prof_phwh">
+                    <div id="user_photo"></div>
+                    <div id="user_who"></div>
+                </div>
+                <div class="user_in">
+                    <div id="user_name"></div>
+
+                    <div id="user_balance"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="head_preview">
+
+        <div class="head_text">
+            <div class="top_text">Найти эксперта<br>быстро и просто</div>
+            <div class="bottom_text">получите консультацию от проверенных экспертов анонимно, позвонив<br>или написав письмо</div>
+            <a href="#" class="head_button">НАЙТИ ЭКСПЕРТА</a>
+            <a href="#" class="to_down"><i class="fas fa-sort-down"></i></a>
+
+        </div>
+
+    </div>
+
+
+    <div class="how_work">
+        <h1>Как это работает</h1>
+
+        <center>
+            <div class="how_blocks">
+                <div class="how_block"><i class="fas fa-user-graduate"></i>
+                    <h2>Зарегистрируйтесь на сайте
+                    </h2>
+                </div>
+                <div class="how_block"><i class="fas fa-question"></i>
+                    <h2>Задайте вопрос эксперту</h2>
+                </div>
+                <div class="how_block"><i class="fas fa-money-bill"></i>
+                    <h2>Оплатите услугу после согласия эксперта, по установленной экспертом ставке</h2>
+                </div>
+                <div class="how_block"><i class="fas fa-briefcase"></i>
+                    <h2>Получите решение вашей проблемы по телефону, в письме, в чате</h2>
+                </div>
+
+            </div>
+
+            <div class="how_block1">
+                <div class="how_block1_1">
+                    <h1>ПЫТАЕТЕСЬ ИСКАТЬ ЭКСПЕРТОВ В ИНТЕРНЕТЕ?</h1>
+                    <h3>Мы сделали онлайн-консультации простыми.</h3>
+                    <h3>Не нужно читать сотни блогов и тратить много времени, чтобы ответить на пару вопросов. Просто зайдите в нашу базу экспертов, выберите лучшего для своих задач и быстро получите консультацию.</h3>
+                    <a href="#" class="tn">
+                        <div class="how_button">Написать эксперту</div>
+                    </a>
+                </div>
+
+                <div class="how_block1_2">
+                    <div class="how_ph1"></div>
+                    <div class="how_ph2"></div>
+                    <div class="how_ph3"></div>
+                    <div class="how_ph4"></div>
+                </div>
+
+            </div>
+        </center>
+
+        <h1>Мы помогаем решать проблемы</h1>
+        <h2>Поддерживаем наших клиентов в быстром и<br> успешном развитии бизнеса</h2>
+
+        <div class="experts_gl">
+            <div class="experts_gl_block">
+                <div class="expert1"><i class="fas fa-circle online"></i></div>
+                <div class="expert_info">
+                    <div class="expert_name">Василенко Алексей</div>
+
+                    <div class="expert_prof">
+                        Бизнес консультации. Управление инновациями в компании. Стратегический маркетинг.
+                        Степени MBA, BBA, CFA
+                        #бизнесконсультант #бизнесэксперт #маркетинг #PR
+
+                        <a href="#" class="tn expert_button"><div class="">Написать эксперту</div></a>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="experts_gl_block">
+                <div class="expert2"><i class="fas fa-circle online" style="color: red;"></i></div>
+                <div class="expert_info">
+                    <div class="expert_name">Смирнов Василий</div>
+
+                    <div class="expert_prof">
+                        Управление проектами в сфере строительства. 12 лет работы в ОАО "МостСтройОтряд". Консультирование по стройматериалам.
+                        #строительство #стройматериалы #управление #учётвстроительстве
+
+                        <a href="#" class="tn expert_button"><div class="">Написать эксперту</div></a>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="experts_gl_block">
+                <div class="expert3"><i class="fas fa-circle online"></i></div>
+                <div class="expert_info">
+                    <div class="expert_name">Григорьев Лев</div>
+
+                    <div class="expert_prof">
+                        Консультации по отношениям с фондами. Бизнес ангел на протяжении 5 лет. Ментор Сколково. Помощь в получении инвестиций.
+                        #бизнесконсультант #бизнесэксперт #инвестиции #фонд
+
+                <a href="#" class="tn expert_button" id="minus"><div class="">Написать эксперту</div></a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <h1 style="padding-top: 30px;">В КАКОЙ СФЕРЕ МЫ МОЖЕМ ВАМ ПОМОЧЬ</h1>
+        <center>
+            <div class="list_help">
+                <a href="#" class="block_help"><i class="fas fa-tools"></i>
+                    <h2>Промышленное производство
+                    </h2>
+                </a>
+
+                <a href="#" class="block_help"><i class="fas fa-project-diagram"></i>
+                    <h2>Бизнес
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-user-tie"></i>
+                    <h2>Карьера
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-book"></i>
+                    <h2>Образование
+                    </h2>
+                </a>
+
+                <a href="#" class="block_help"><i class="fas fa-birthday-cake"></i>
+                    <h2>Организация мероприятий
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-file-invoice-dollar"></i>
+                    <h2>Финансы
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-running"></i>
+                    <h2>Фитнес
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-heartbeat"></i>
+                    <h2>Здоровье
+                    </h2>
+                </a>
+
+                <a href="#" class="block_help"><i class="fas fa-bullhorn"></i>
+                    <h2>PR & РЕКЛАМА
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-balance-scale"></i>
+                    <h2>Законодательство
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-door-closed"></i>
+                    <h2>Недвижимость
+                    </h2>
+                </a>
+                <a href="#" class="block_help"><i class="fas fa-plane"></i>
+                    <h2>Иммиграция
+                    </h2>
+                </a>
+
+
+                <a href="#" class="expert_button m60 full">ВСЕ КАТЕГОРИИ</a>
+
+            </div>
+        </center>
+
+        <div class="block_reviews">
+            <h1>ОТЗЫВЫ О НАС</h1>
+            <center>
+                <div class="revs">
+                    <div class="rev">
+                        <div class="rev1"></div>
+                        <div class="rev_text">
+                            <h3>Вы профессионалы! Спасибо за уделенное время на решение проблем. Качественно, быстро, эффективно.</h3>
+                            <div class="rev_name">Николай Пономарев</div>
+                            <div class="rev_dol">Генеральный директор <span>ООО "АгроПром"</span></div>
+                        </div>
+                    </div>
+                    <div class="rev">
+                        <div class="rev2"></div>
+                        <div class="rev_text">
+                            <h3>Мы узнали много нового и получили очень дельные советы. Очень рекомендуем обращаться к Олегу Соболеву!</h3>
+                            <div class="rev_name">Даниил Калиниченко</div>
+                            <div class="rev_dol">Финансовый менеджер в <span>DD agency</span></div>
+                        </div>
+                    </div>
+                    <div class="rev">
+                        <div class="rev3"></div>
+                        <div class="rev_text">
+                            <h3>Быстро решили вопрос. Помогли с решением ряда проблем, ответы на которые не найдешь на других ресурсах. Спасибо!</h3>
+                            <div class="rev_name">Юлия Санина</div>
+                            <div class="rev_dol">Основатель <span>Школа программирования "IQ-школа"</span></div>
+                        </div>
+                    </div>
+                    <div class="rev">
+                        <div class="rev4"></div>
+                        <div class="rev_text">
+                            <h3>Идеальный сервис для меня. Грамотные эксперты и быстрое обслуживание.</h3>
+                            <div class="rev_name">Саша Коротких</div>
+                            <div class="rev_dol">Креативный директор <span>SoSoul magazine</span></div>
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+
+        <h1>Остались вопросы?</h1>
+        <h2 class="mgb60">Напиши нам! Оставьте отзыв</h2>
+        <center>
+            <div class="feedback">
+                <div class="feedback_left">
+                    <div class="number">+7 903 40 777 42</div>
+                    <div class="mail">hello@madeontilda.com</div>
+                    <div class="address">Ростов-на-Дону, пл. Гагарина 1, к 2</div>
+                    <div class="icons"><i class="fab fa-twitter-square"></i><i class="fab fa-facebook-square"></i><i class="fab fa-instagram"></i></div>
+                </div>
+                <div class="feedback_right">
+                    <input type="text" placeholder="Ваше имя">
+                    <input type="text" placeholder="Ваш E-mail">
+                    <textarea id="feed_text" placeholder="Напишите свой вопрос"></textarea>
+                    <a href="#" class="expert_button mgt-60">Отправить письмо</a>
+                </div>
+            </div>
+        </center>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
